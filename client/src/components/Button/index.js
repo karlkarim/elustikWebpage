a6e4e02713441ea5ef376aa190b2fbcf2d4812f3
karlkarim <karl.karimoisa@tmd.ee>
2020-04-03 12:36:37 +0300

    add general button

---
+++ client/src/components/Button/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+const ButtonB = ({label, onClick}) => {
+
+    return(
+    <button className='button-general'>{label}</button>
+    )
+}
+export default ButtonB;
